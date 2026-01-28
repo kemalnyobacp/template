8417997c137fa6235a6b8034f8c8dc21b8b71479
ardofulian <careday17@gmail.com>
2026-01-28 16:21:29 +0700

    update version

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -10,6 +10,9 @@
 #define debug(x) cerr << #x << " = " << x << endl;
 #define debug2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl;
 #define debug3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl;
+#define file_input "shell.in"
+#define file_output "shell.out"
+#define usaco 1
 
 using namespace std;
 
@@ -26,11 +29,29 @@ void solve() {
     }
 }
 
+void readInputUsaco() {
+    freopen("shell.in", "r", stdin);
+}
+
+void writeOutputUsaco() {
+    freopen("shell.out", "w", stdout);
+}
+
+void runUsaco() {
+    readInputUsaco();
+    solve();
+    writeOutputUsaco();
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    solve();
+    if (usaco) {
+        runUsaco();
+    } else {
+        solve();
+    }
 
     return 0;
 }
