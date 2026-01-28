67679c01ec68e3994b484cc00de938ee995d1e1e
ardofulian <careday17@gmail.com>
2026-01-28 16:13:35 +0700

    update version

---
+++ cpp/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Run() {
+    cout << "0" << "\n";
+}
+
+void Solve() {
+    int t;
+    cin >> t;
+    while (t > 0) {
+        Run();
+        t--;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solve();
+
+    return 0;
+}
